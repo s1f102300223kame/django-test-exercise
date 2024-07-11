7e6a88c56eed2117d334978f23d24a60d0954d8c
1F10230022 <s1f102300223@iniad.org>
2024-07-11 14:12:11 +0900

    追加

--- todo/tests.py
+++ todo/tests.py
@@ -46,7 +46,7 @@ class TaskModelTestCase(TestCase):
 
     def test_is_overdue_none(self):
         current = timezone.make_aware(datetime(2024, 7, 1, 0, 0, 0))
-        task = Task(title='task1', due_at=due)
+        task = Task(title='task1')
         task.save()
         self.assertFalse(task.is_overdue(current))
 
